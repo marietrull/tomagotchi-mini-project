b0c84b616e5dedbe6b3a49ac8fb89439ed1d90aa
marietrull <marietrull@gmail.com>
2018-03-27 14:26:56 -0500

    Commit 2: Displayed Metrics to the Tomagatchi

--- tomagotchi.js
+++ tomagotchi.js
@@ -1,1 +1,42 @@
 console.log("I am working on the tomagotchi mini-project.");
+
+//Add Hunger to the Tomagotchi
+const addHunger = () => {
+	let hunger = 1;
+	const $hungerDiv = $('<div>');
+	$hungerDiv.text("Hunger: " + hunger);
+	$hungerDiv.appendTo($('.tomagotchi'));
+}
+
+addHunger();
+
+//Add Sleepiness to the Tomagotchi
+const addSleepy = () => {
+	let sleepiness = 1;
+	const $sleepyDiv = $('<div>');
+	$sleepyDiv.text("Sleepiness: " + sleepiness);
+	$sleepyDiv.appendTo($('.tomagotchi'));
+}
+
+addSleepy();
+
+//Add Boredom to the Tomagotchi
+const addBoredom = () => {
+	let boredom = 1;
+	const $boredDiv = $('<div>');
+	$boredDiv.text("Boredom: " + boredom);
+	$boredDiv.appendTo($('.tomagotchi'));
+}
+
+addBoredom();
+
+//Add Age to the Tomagotchi
+
+const addAge = () => {
+	let age = 0;
+	const $ageDiv = $('<div>');
+	$ageDiv.text("Age: " + age);
+	$ageDiv.appendTo($('.tomagotchi'));
+}
+
+addAge();
