fe26f0c10e3028f8c07a06fdc57c70f50777a2fe
marietrull <marietrull@gmail.com>
2018-03-27 14:49:02 -0500

    Commit 3: Added Buttons + Ability to Name Tomagatchi

--- tomagotchi.js
+++ tomagotchi.js
@@ -1,8 +1,19 @@
 console.log("I am working on the tomagotchi mini-project.");
 
+
+//Add Name to the Tomagotchi
+const addName = () => {
+	const name = prompt("A Tomagotchi has been born! What is it's name?", "Enter Name");
+	const $nameDiv = $('<div>');
+	$nameDiv.text("Name: " + name);
+	$nameDiv.appendTo($('.tomagotchi'));
+}
+
+addName();
+
 //Add Hunger to the Tomagotchi
 const addHunger = () => {
-	let hunger = 1;
+	let hunger = 5;
 	const $hungerDiv = $('<div>');
 	$hungerDiv.text("Hunger: " + hunger);
 	$hungerDiv.appendTo($('.tomagotchi'));
@@ -40,3 +51,29 @@ const addAge = () => {
 }
 
 addAge();
+
+//Feed Button Functionality
+
+$('#feed').on('click', () => {
+	console.log('Feed Button Clicked.'); // Test to make sure it's on and working
+})
+
+//Play Button Functionality
+
+$('#play').on('click', () => {
+	console.log('Play Button Clicked.'); // Test to make sure it's on and working
+})
+
+//Light Switch Button Functionality
+
+$('#light').on('click', () => {
+	console.log('Light Switch Button Clicked.'); // Test to make sure it's on and working
+})
+
+
+
+
+
+
+
+
