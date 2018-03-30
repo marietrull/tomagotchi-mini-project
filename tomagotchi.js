500242e0364a13b5dc5b7bc59b254c7b98608659
marietrull <marietrull@gmail.com>
2018-03-29 22:39:41 -0500

    Commit 10: Updated JS and CSS

--- tomagotchi.js
+++ tomagotchi.js
@@ -1,139 +1,136 @@
 console.log("I am working on the tomagotchi mini-project.");
-let hunger = 5;
-let sleepiness = 5;
-let boredom = 5;
-let age = 0;
-let status = 'Baby';
+
 const statArray = ['Baby', 'Tween', 'Grown-Up', 'Old-Timer'];
 
-//Add Name to the Tomagotchi
-const addName = () => {
-	const name = prompt("A Tomagotchi has been born! What is it's name?", "Enter Name");
-	const $nameDiv = $('<div>');
-	$nameDiv.text("Name: " + name);
-	$nameDiv.appendTo($('.tomagotchi'));
-}
-
-addName();
-
-//Add Hunger to the Tomagotchi
-const addHunger = () => {
-	const $hungerDiv = $('<div>');
-	$hungerDiv.text("Hunger: " + hunger);
-	$hungerDiv.appendTo($('.tomagotchi'));
-	$('#feed').on('click', () => {
-		hunger -= 1;
-		$hungerDiv.text("Hunger: " + hunger);
-	})
-	//Set Timer
-	const timer = setInterval(() => {
-		hunger += 1; 
-		$hungerDiv.text("Hunger: " + hunger);
-		if (hunger === 10){
-			clearInterval(timer);
-			window.alert("Your Tomogatchi starved and is now dead.");
-		}
-		
-	}, 60000);
-}
-
-addHunger();
-
-//Add Sleepiness to the Tomagotchi
-const addSleepy = () => {
-	const $sleepyDiv = $('<div>');
-	$sleepyDiv.text("Sleepiness: " + sleepiness);
-	$sleepyDiv.appendTo($('.tomagotchi'));
-	//Make light switch functional
-	$('#light').on('click', () => {
-		let $background = $('.tomagotchi');
-		// Turn light off
-		if($background.css('background-color')===('rgb(255, 255, 255)')){
-			$background.css('background-color', '#3d3d3d');
-			$background.css('color', 'white');
-			// Tomagatchi becomes less sleepy every time you turn the light off and let it "sleep".
-			sleepiness -= 1;
-			$sleepyDiv.text("Sleepiness: " + sleepiness);
-			console.log(sleepiness);
-			// Turn the light back on. This has no affect on the Tomagatchi. 
-		} else {
-			$background.css('background-color', 'white');
-			$background.css('color', '#3d3d3d');
-		}
-	})
-	const timer = setInterval(() => {
-		sleepiness += 1; 
-		$sleepyDiv.text("Sleepiness: " + sleepiness);
-		if (sleepiness === 10){
-			clearInterval(timer);
-			window.alert("Insomnia has driven your Tomagotchi mad.");
-		}
-	}, 60000);
-}
-
-addSleepy();
-
-//Add Boredom to the Tomagotchi
-const addBoredom = () => {
-	const $boredDiv = $('<div>');
-	$boredDiv.text("Boredom: " + boredom);
-	$boredDiv.appendTo($('.tomagotchi'));
-		//Make play switch functional
-	$('#play').on('click', () => {
-		// Tomagatchi becomes less bored by one point every time you play with it.
-		boredom -= 1;
-		$boredDiv.text("Boredom: " + boredom);
-	})
-	const timer = setInterval(() => {
-		boredom += 1; 
-		$boredDiv.text("Boredom: " + boredom);
-		if (boredom === 10){
-			clearInterval(timer);
-			window.alert("Boredom has taken over your Tomagotchi - it isn't dead - but it might as well be.");
-		}
-	}, 60000);
-
-}
-
-addBoredom();
 
 
-//Add Status to the Tomagotchi
+class Tamagotchi {
+	constructor(hunger, sleepiness, boredom, age, status){
+		this.hunger = hunger;
+		this.sleepiness = sleepiness;
+		this.boredom = boredom;
+		this.age = age;
+		this.status = status;
 
-const addStatus = () => {
-	const $statusDiv = $('<div>');
-	$statusDiv.text("Stage in Life: " + status);
-	$statusDiv.appendTo($('.tomagotchi'));
-	let statNum = 0
-	const timer = setInterval(() => {
-		statNum += 1;
-		status = statArray[statNum];
-		$statusDiv.text("Stage in Life: " + status);
-		if (statNum === 3){
-			clearInterval(timer);
-		}	
-	}, 60000);
+	}
+	addName(){
+		const name = prompt("A Tomagotchi has been born! What is it's name?", "Enter Name");
+		$('#name').text("Name: " + name);
+	}
+	addHunger(){
+		$('#hunger').text("Hunger: " + this.hunger);
+	}
+	addSleepy(){
+		$('#sleepy').text("Sleepiness: " + this.sleepiness);
+	}
+	addBoredom(){
+		$('#bored').text("Boredom: " + this.boredom);
+	}
+	addAge(){
+		$('#age').text("Age: " + this.age);
+	}
+	addStatus(){
+		$('#stat').text("Stage in Life: " + this.status);
+	}
+	timer(){
+		let time = 0;
 
-}
+		setInterval(() => {
+			time +=1; 
 
-//Add Age to the Tomagotchi
+			if (time % 10 === 0 && this.hunger < 10 && this.sleepiness < 10 && this.boredom < 10){
+				this.hunger +=1;
+				$('#hunger').text("Hunger: " + this.hunger);
+				this.sleepiness +=1;
+				$('#sleepy').text("Sleepiness: " + this.sleepiness);
+				this.boredom +=1;
+				$('#bored').text("Boredom: " + this.boredom);
+			} 
+			console.log(time);
 
-const addAge = () => {
-	const $ageDiv = $('<div>');
-	$ageDiv.text("Age: " + age);
-	$ageDiv.appendTo($('.tomagotchi'));
-	addStatus();
-	const timer = setInterval(() => {
-		age += 1; 
-		$ageDiv.text("Age: " + age);
-		
-	}, 60000);
+			if (time % 60 === 0){
+				this.age +=1;
+				$('#age').text("Age: " + this.age);
+			}
+
+		}, 1000);
+	}
+};
 
 
+//GIVE FEED BUTTON FUNCTIONALITY
+$('#feed').on('click', () => {
+	myTom.hunger -= 1;
+	$('#hunger').text("Hunger: " + myTom.hunger);
+});
 
-}
 
-addAge();
+//GIVE LIGHT SWITCH BUTTON FUNCTIONALITY
+$('#light').on('click', () => {
+	let $wholePage = $('body');
+	let $background = $('#tomagotchi');
+	let $header = $('header');
+	// Turn light off
+	if($background.css('background-color')===('rgb(255, 255, 255)')){
+		// Change css features with the lights off
+		$wholePage.css('background-color', 'black');
+		$background.css('background-color', '#3d3d3d');
+		$background.css('color', 'white');
+		$header.css('background-color', "#3d3d3d");
+		$header.css('color', 'white');
+		// Switch image when light is off
+		$('#photo').attr('src','http://de.tamagotchifriends.com/wp-content/uploads/sites/10/2013/11/kuchipatchi-info2.png');
+		// Tomagatchi becomes less sleepy every time you turn the light off and let it "sleep".
+		myTom.sleepiness -= 1;
+		$('#sleepy').text("Sleepiness: " + myTom.sleepiness);
+		// Turn the light back on. This has no affect on the Tomagatchi. 
+	} else {
+		// change css features with the lights on
+		$background.css('background-color', 'white');
+		$wholePage.css('background-color', '#00CDCD');
+		$background.css('color', '#3d3d3d');
+		$header.css('background-color', "white");
+		$header.css('color', '#3d3d3d');
+		// switch image when the light is on
+		$('#photo').attr('src','https://vignette.wikia.nocookie.net/tamagotchi/images/1/12/Kuchipatchi_pose.png/revision/latest?cb=20140831175650');
+	}
+});
+
+//GIVE PLAY BUTTON FUNCTIONALITY
+$('#play').on('click', () => {
+	// Tomagatchi becomes less bored by one point every time you play with it.
+	myTom.boredom -= 1;
+	$('#bored').text("Boredom: " + myTom.boredom);
+});
+
+//GIVE RESET BUTTON FUNCTIONALITY
+$('#reset').on('click', () => {
+	myTom.hunger = 5;
+	$('#hunger').text("Hunger: " + myTom.hunger);
+	myTom.sleepiness = 5;
+	$('#sleepy').text("Sleepiness: " + myTom.sleepiness);
+	myTom.boredom = 5;
+	$('#bored').text("Boredom: " + myTom.boredom);
+});
+
+//CALL CLASS + METHODS
+const myTom = new Tamagotchi(5,5,5,0,'Baby');
+
+myTom.addName();
+
+myTom.addHunger();
+
+myTom.addSleepy();
+
+myTom.addBoredom();
+
+myTom.addAge();
+
+myTom.addStatus();
+
+// myTom.timer();
+
+
 
 
 
